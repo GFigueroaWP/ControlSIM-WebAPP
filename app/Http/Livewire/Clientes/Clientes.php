612b12f02581cd10a5e9917f8c9c125a3207c49c
Guido Figueroa <gf.kuro.p@gmail.com>
2022-11-22 15:43:48 -0300

    funcionalidad crear cliente

--- app/Http/Livewire/Clientes/Clientes.php
+++ app/Http/Livewire/Clientes/Clientes.php
@@ -31,6 +31,8 @@ class Clientes extends Component
         $this->modalCreacionCliente = false;
     }
 
+    public $cli_nombre, $cli_razonsocial, $cli_rut, $cli_email, $cli_telefono, $cli_direccion, $cli_comuna, $cli_region;
+
     public function formatRut()
     {
         $cli_rut = $this-> cli_rut;
@@ -50,5 +52,44 @@ class Clientes extends Component
         $this->cli_rut = $formatted;
     }
 
+    protected $rules = [
+        'cli_nombre' => 'required|alpha_num',
+        'cli_razonsocial' => 'required|alpha_num',
+        'cli_rut' => 'required|size:10',
+        'cli_email' => 'email',
+        'cli_telefono' => 'numeric',
+        'cli_direccion' => 'alpha_num',
+        'cli_comuna' => 'alpha_num',
+        'cli_region' => 'alpha_num'
+    ];
 
+    protected $messages = [
+        'cli_nombre',
+        'cli_razonsocial',
+        'cli_rut',
+        'cli_email',
+        'cli_telefono',
+        'cli_direccion',
+        'cli_comuna',
+        'cli_region'
+    ];
+
+    public function submit()
+    {
+        $this->validate();
+        Cliente::create([
+            'cli_nombre' => $this->cli_nombre,
+            'cli_razonsocial' => $this->cli_razonsocial,
+            'cli_rut' => $this->cli_rut,
+            'cli_email' => $this->cli_email,
+            'cli_telefono' => $this->cli_telefono,
+            'cli_direccion' => $this->cli_direccion,
+            'cli_comuna' => $this->cli_comuna,
+            'cli_region' => $this->cli_region
+        ]);
+        session()->flash('flash.banner', 'Nuevo cliente añadido con éxito');
+        session()->flash('flash.bannerStyle', 'success');
+        $this->modalCreacionCliente = false;
+        return redirect()->to('/clientes');
+    }
 }
