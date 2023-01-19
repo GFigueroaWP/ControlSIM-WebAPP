6368ab8bab52ed5fc9bab858fe486f97feea942b
Guido Figueroa <gf.kuro.p@gmail.com>
2023-01-19 17:49:14 -0300

    datatable rappasoft

---
+++ app/Http/Livewire/Empleados/EmpleadoTable.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Livewire\Empleados;
+
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use App\Models\User;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Rappasoft\LaravelLivewireTables\Views\Columns\ButtonGroupColumn;
+use Rappasoft\LaravelLivewireTables\Views\Columns\LinkColumn;
+
+class EmpleadoTable extends DataTableComponent
+{
+    protected $model = User::class;
+
+    public function configure(): void
+    {
+        $this->setPrimaryKey('id');
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make("Id", "id")
+                ->sortable(),
+            Column::make("Rut", "us_rut")
+                ->sortable()
+                ->searchable(),
+            Column::make("Nombre de usuario", "us_username")
+                ->sortable()
+                ->searchable(),
+            Column::make("Nombre", "us_nombre")
+                ->sortable()
+                ->searchable(),
+            Column::make("Apellido", "us_apellido")
+                ->sortable()
+                ->searchable(),
+            ButtonGroupColumn::make('Acciones')
+                ->attributes(function($row) {
+                    return [
+                        'class' => 'space-x-2',
+                    ];
+                })
+                ->buttons([
+                    LinkColumn::make('Ver')
+                        ->title(fn($row) => 'Ver')
+                        ->location(fn($row) => route('showEmpleados', $row->id)),
+                ]),
+            /* Column::make("Us telefono", "us_telefono")
+                ->sortable(),
+            Column::make("Us email", "us_email")
+                ->sortable(),
+            Column::make("Created at", "created_at")
+                ->sortable(),
+            Column::make("Updated at", "updated_at")
+                ->sortable(), */
+        ];
+    }
+}
--- resources/views/livewire/empleados/empleados.blade.php
+++ resources/views/livewire/empleados/empleados.blade.php
@@ -24,21 +24,11 @@
                         <x-jet-button wire:click="exportEmpleado">{{ __('Exportar Empleados') }}</x-jet-button>
                         @endcan
                     </div>
-                    <label for="search_empleados" class="sr-only">Buscar</label>
-                    <div class="relative justify-self-end">
-                        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor"
-                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </div>
-                        <input wire:model='filtro_us' class="block p-2 pl-10 w-80 border border-gray-300" type="text"
-                            name="search_empleados" id="search_empleados" placeholder="Buscar Empleados">
-                    </div>
                 </div>
-                <table class=" w-full text-base text-left">
+                <div class="m-5">
+                    <livewire:empleados.empleado-table />
+                </div>
+                {{-- <table class=" w-full text-base text-left">
                     <thead class="uppercase">
                         <tr>
                             <th scope="col" class="py-2 px-6">
@@ -89,10 +79,11 @@
                 <div class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
                     No hay resultados para la búsqueda "{{ $filtro_us }}"
                 </div>
-                @endif
+                @endif --}}
             </div>
         </div>
     </div>
+
 
     {{-- Modal de confirmación de suspension de usuario --}}
     <x-jet-confirmation-modal wire:model='modalDeshabilitacionEmpleado'>
