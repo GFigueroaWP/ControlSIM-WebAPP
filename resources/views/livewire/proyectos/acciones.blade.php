94459bdb9b71ee185fd785ff13481a7257b82953
Guido Figueroa <gf.kuro.p@gmail.com>
2023-01-20 13:06:04 -0300

    ds

--- resources/views/livewire/proyectos/acciones.blade.php
+++ resources/views/livewire/proyectos/acciones.blade.php
@@ -1,1 +1,1 @@
-<x-jet-button wire:click="$emit('showProgreso',{{ $cotizacion->id }},{{ $cotizacion->trabajo->id }})">Ver</x-jet-button>
+<x-jet-button wire:click="$emit('showProgreso',{{ $cotizacion->id }},{{ $cotizacion->trabajo->id ?? '' }})">Ver</x-jet-button>
