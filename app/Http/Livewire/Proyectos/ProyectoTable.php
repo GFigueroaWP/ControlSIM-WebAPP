51c41ccdd82838376d6015a5975afb4c46631af6
Guido Figueroa <gf.kuro.p@gmail.com>
2023-01-20 13:03:05 -0300

    dsa

--- app/Http/Livewire/Proyectos/ProyectoTable.php
+++ app/Http/Livewire/Proyectos/ProyectoTable.php
@@ -40,13 +40,13 @@ class ProyectoTable extends DataTableComponent
             Column::make("Trabajo", "trabajo.id")
                 ->sortable()
                 ->searchable()->format(
-                    fn($value, $row, Column $column) => 'OT-'.str_pad($value,5,'0',STR_PAD_LEFT)
+                    fn($value, $row, Column $column) => 'OT-'.str_pad($value ?? ' ',5,'0',STR_PAD_LEFT)
                 ),
             Column::make("Estado de trabajo", "trabajo.ot_estado")
                 ->sortable()
                 ->searchable()
                 ->format(
-                    fn($value, $row, Column $column) => '<strong class="'.$value.'">'.$value.'</strong>'
+                    fn($value, $row, Column $column) => '<strong class="'.$value ?? ' '.'">'.$value ?? ' '.'</strong>'
                 )
                 ->html(),
             Column::make("Acciones")
