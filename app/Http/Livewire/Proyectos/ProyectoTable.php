6fe473109452ee8f6e2302a5fce29b1dcecb1552
Guido Figueroa <gf.kuro.p@gmail.com>
2023-01-20 12:27:31 -0300

    arreglo

--- app/Http/Livewire/Proyectos/ProyectoTable.php
+++ app/Http/Livewire/Proyectos/ProyectoTable.php
@@ -40,7 +40,7 @@ class ProyectoTable extends DataTableComponent
             Column::make("Trabajo", "trabajo.id")
                 ->sortable()
                 ->searchable()->format(
-                    fn($value, $row, Column $column) => 'OT-'.str_pad($row->trabajo->id,5,'0',STR_PAD_LEFT)
+                    fn($value, $row, Column $column) => 'OT-'.str_pad($row->trabajo->id,5,'0',STR_PAD_LEFT) ?? ''
                 ),
             Column::make("Estado de trabajo", "trabajo.ot_estado")
                 ->sortable()
